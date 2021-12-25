a9db04742336bca6a157165fbf2574e288a234a3
leadtheninjazz <91322451+leadtheninjazz@users.noreply.github.com>
2021-12-25 17:57:11 +0700

    First commit
    you may cringe really hard reading this

---
+++ main.py
@@ -0,0 +1,42 @@
+
+#print
+#print("Hello World!")
+#print("o----")
+#print(" |||| ")
+#print(" lah dep trai "* 10000000)
+
+#var
+#height = 160
+#name = 'lah'
+#is_new = True
+#print(height)
+#print(name)
+#print(is_new)
+
+#inputs
+#name = input(" what is your name u mf? " )
+#color = input(" what is your fav color u mf? " )
+#print(name + " fav color is " + color)
+
+#conversions
+#name = input(" What is your name u mf: ")
+#year = input(" what year were u born in: ")
+#age = 2021 - int(year)
+#print (name + " is")
+#print (age)
+#weight_kilo = input("what is your weight in kilo: ")
+#weight_pounds = int(weight_kilo) / 0.45
+#print ("your weight in pounds is: ")
+#print ( weight_pounds )
+
+#strings
+#deptrai = "lah dep trai so 1 tg"
+#print(deptrai [0:3])
+
+#formated strings
+first = "Anh Huy"
+last = "Le"
+#message = first + ' [' + last + '] is a coder'
+#print(message)
+msg = f'{first} [{last}] is a coder'
+print(msg)
